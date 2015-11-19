bfccd47dd6c7f1c210908128d76fa749b836c8ef
Halliax <tmsheneman@gmail.com>
2015-11-19 00:29:08 -0500

    Simulation function is now specific to bolas. Also, completely incorrect in every way.

--- simulateBolas.m
+++ simulateBolas.m
@@ -5,9 +5,10 @@ initialY = InitialConditions(2); % m
 initialVx = InitialConditions(3); % m/s
 initialVy = InitialConditions(4); % m/s
 
+r = .75; % m
 g = 9.81; % m/s
 
-[Times, Stocks] = ode45(@pendulumDerivs, [0, endTime], [initialX, initialY, initialVx, initialVy]);
+[Times, Stocks] = ode45(@bolasDerivs, [0, 5], [initialX, initialY, initialVx, initialVy]);
 Xpositions = Stocks(:,1);
 Ypositions = Stocks(:,2);
 Xvelocities = Stocks(:,3);
@@ -16,29 +17,27 @@ Yvelocities = Stocks(:,4);
 fig1 = figure();
 fig1.OuterPosition = [10,200,570,510];
 comet(Times,Xpositions);
-title('Pendulum X Position over Time');
+title('Bolas X Position over Time');
 
 fig2 = figure();
 fig2.OuterPosition = [600,200,570,510];
 comet(Times,Ypositions);
-title('Pendulum Y Position over Time');
+title('Bolas Y Position over Time');
 
 fig3 = figure();
 fig3.OuterPosition = [1190,200,570,510];
 comet(Xpositions,Ypositions);
-title('Pendulum Position over Time');
+title('Bolas Position over Time');
 
-    function res = pendulumDerivs(~, S)
+    function res = bolasDerivs(~, S)
     X = S(1);
     Y = S(2);
     Vx = S(3);
     Vy = S(4);
     
-    dVx = (- springConstant * (sqrt(X^2+Y^2) - restLength) * ...
-        (X / (sqrt(X^2+Y^2)))) / mass;
+    dVx = (Vx^2 + Vy^2) / r * (X / sqrt(X^2 + Y^2));
 
-    dVy = (-mass * g  - springConstant * (sqrt(X^2+Y^2) - restLength) * ...
-        (Y / (sqrt(X^2+Y^2)))) / mass;
+    dVy = (Vx^2 + Vy^2) / r * (Y / sqrt(X^2 + Y^2));
 
     res = [Vx; Vy; dVx; dVy];
 
